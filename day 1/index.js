368eee246a8a30efc4b426528a9faf825d284e8b
isha kandel <ishachttri@gmail.com>
2025-05-21 23:15:48 +0545

    Refactor MongoDB connection handling with promise-based approach

--- day 1/index.js
+++ day 1/index.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+main()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Connection Error:", err);
+  });
