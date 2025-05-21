66ba906f20172895cccb187ae70ac364fe0bbdee
isha kandel <ishachttri@gmail.com>
2025-05-21 23:16:07 +0545

    done

--- day 1/index.js
+++ day 1/index.js
@@ -7,3 +7,9 @@ main()
   .catch((err) => {
     console.log("Connection Error:", err);
   });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  // If auth enabled, use:
+  // await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');
+}
